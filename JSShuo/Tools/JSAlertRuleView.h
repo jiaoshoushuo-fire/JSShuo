d25c11bb1f7ffe3d4a61d28c7808c2905b6e98e7
xiaozhongxiang <qiaozhongxiang@waibao.com>
2018-12-22 23:58:48 +0800

    bug fix

---
+++ JSShuo/Tools/JSAlertRuleView.h
@@ -0,0 +1,17 @@
+//
+//  JSAlertRuleView.h
+//  JSShuo
+//
+//  Created by  乔中祥 on 2018/12/22.
+//  Copyright © 2018年  乔中祥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JSAlertRuleView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
